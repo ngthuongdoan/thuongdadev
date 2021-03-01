84796e120ed242d066e6059b6ca7cd0dc1f6f870
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-01 13:45:28 +0700

    style: add social button

---
+++ components/SocialPane.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="social__pane">
+    <ul class="social__list">
+      <li>
+        <a
+          href="https://www.facebook.com/MaFamille.Cafeteria"
+          target="_blank"
+        ></a>
+      </li>
+      <li>
+        <a href="mailto:mafamille90@gmail.com" target="_blank"></a>
+      </li>
+      <li>
+        <a href="tel:07103819432"></a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.social {
+  &__pane {
+    position: fixed;
+    width: max-content;
+    background-color: white;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    box-shadow: -1px 1px 2px rgba(0, 0, 0, 0.1);
+    z-index: 10;
+  }
+  &__list {
+    list-style-type: none;
+    margin: 10px auto;
+    li {
+      margin: 15px 10px;
+    }
+    img {
+      width: 20px;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="relative h-screen w-screen min-h-screen min-w-screen">
+    <div class="fixed flex top-5 right-5 space-x-4">
+      <a href="">
+        <img
+          class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
+          src="https://img.icons8.com/fluent/48/000000/facebook-new.png"
+        />
+      </a>
+
+      <img
+        class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
+        src="https://img.icons8.com/fluent/48/000000/gmail--v1.png"
+      />
+      <img
+        class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
+        src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+      />
+    </div>
     <Nuxt />
   </div>
 </template>
 
 <script>
+import Social from '@/components/SocialPane'
+export default {
+  components: {
+    Social,
+  },
+}
 </script>
 <style lang="scss">
 /* Enter and leave animations can use different */
@@ -17,5 +40,11 @@
 /* .slide-fade-leave-active below version 2.1.8 */ {
   transform: translateX(-100%);
   opacity: 0.8;
+}
+.grayscale {
+  filter: grayscale(10);
+  &:hover {
+    filter: grayscale(0);
+  }
 }
 </style>
