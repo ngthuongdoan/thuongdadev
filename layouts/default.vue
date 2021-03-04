2411f99f35f0b603c4bc0c7cad35f916d40885ca
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-04 14:16:18 +0700

    feat: add fullpage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="relative h-screen w-screen min-h-screen min-w-screen">
     <div class="fixed flex top-5 right-5 space-x-4">
-      <a href="">
+      <a href="https://github.com/ngthuongdoan" target="_blank">
+        <img
+          class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
+          src="https://img.icons8.com/fluent/96/000000/github.png"
+        />
+      </a>
+
+      <a href="https://www.facebook.com/thuongdadev" target="_blank">
         <img
           class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
           src="https://img.icons8.com/fluent/48/000000/facebook-new.png"
         />
       </a>
-
-      <img
-        class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
-        src="https://img.icons8.com/fluent/48/000000/gmail--v1.png"
-      />
-      <img
-        class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
-        src="https://img.icons8.com/fluent/48/000000/linkedin.png"
-      />
+      <a href="mailto:ngthuongdoan@gmail.com" target="_blank">
+        <img
+          class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
+          src="https://img.icons8.com/fluent/48/000000/gmail--v1.png"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/ngthuongdoan/" target="_blank">
+        <img
+          class="w-7 object-contain transition-all duration-300 ease-in-out cursor-pointer grayscale"
+          src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+        />
+      </a>
     </div>
     <Nuxt />
   </div>
