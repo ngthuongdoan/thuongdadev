863fcd4ca65565dd1d26da5180341596940afe87
ngthuongdoan <ngthuongdoan@gmail.com>
2021-03-08 02:44:37 +0700

    style: coming soon

--- components/section/ComingSoon.vue
+++ components/section/ComingSoon.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex bg-sunbrust justify-center items-center w-screen h-screen">
-    <h1 class="text-6xl text-cloud font-bold">Coming Soon</h1>
+    <h1 class="text-6xl text-cloud font-bold pl-5">Coming Soon</h1>
   </div>
 </template>
 
